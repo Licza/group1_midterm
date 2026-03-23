16d3448be6b631fe036107c0de0ecc93087da76e
Emma Dunne <emdunne03@gmail.com>
2026-03-23 11:22:32 -0400

    added emma subproject logistic regression report

---
+++ subproject_emma/code/01_models.R
@@ -0,0 +1,38 @@
+here::i_am("subproject_emma/code/01_models.R")
+
+library(dplyr)
+library(gtsummary)
+library(readr)
+
+#data <- readRDS(
+  #file = here::here("data/covid_sub.csv")
+#)
+
+data <- readr::read_csv(here::here("data/covid_sub.csv"))
+
+data_model <- data |>
+  filter(!is.na(INTUBED), !is.na(AGE)) |>
+  mutate(
+    INTUBED = factor(INTUBED, levels = c("No", "Yes"))
+  )
+
+intubation_age_mod <- glm(
+  INTUBED ~ AGE,
+  data = data_model,
+  family = binomial()
+)
+
+intubation_age_table <- 
+  tbl_regression(
+    intubation_age_mod,
+    exponentiate = TRUE
+  ) |>
+  add_global_p()
+
+saveRDS(
+  list(
+    model = intubation_age_mod,
+    table = intubation_age_table
+  ),
+  file = here::here("subproject_emma/output/intubation_age_results.rds")
+)
